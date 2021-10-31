8f085efb1459eb823f2e28c2c29f527ab55ad7e9
Arnav Shah <shaharnav03@gmail.com>
2021-10-31 13:44:11 -0400

    Implement sacrifice heuristic

--- agents/sacrifice.py
+++ agents/sacrifice.py
@@ -3,11 +3,21 @@ from lux.game import Game
 from lux.game_map import Cell, RESOURCE_TYPES
 from lux.constants import Constants
 from lux.game_constants import GAME_CONSTANTS
+from lux.game_objects import Unit
 from lux import annotate
 
 DIRECTIONS = Constants.DIRECTIONS
 game_state = None
 
+def get_closest_tile(tiles: list[Cell], unit: Unit):
+    closest_dist = math.inf
+    closest_tile = None
+    for tile in tiles:
+        dist = tile.pos.distance_to(unit.pos)
+        if dist < closest_dist:
+            closest_dist = dist
+            closest_tile = tile
+    return closest_tile
 
 def agent(observation, configuration):
     global game_state
@@ -28,52 +38,33 @@ def agent(observation, configuration):
     opponent = game_state.players[(observation.player + 1) % 2]
     width, height = game_state.map.width, game_state.map.height
 
+    empty_tiles: list[Cell] = []
     resource_tiles: list[Cell] = []
     for y in range(height):
         for x in range(width):
             cell = game_state.map.get_cell(x, y)
+            if not (cell.has_resource() or cell.citytile):
+                empty_tiles.append(cell)
             if cell.has_resource():
                 resource_tiles.append(cell)
 
-    # we iterate over all our units and do something with them
-    
-    # Research if city tile is null cooldown
-    if (not player.researched_uranium()):
-        for k, city in player.cities.items():
-            for citytile in city.citytiles:
-                if citytile.can_act():
-                    actions.append(citytile.research())
+    # unit actions
     for unit in player.units:
-        if unit.is_worker() and unit.can_act():
-            closest_dist = math.inf
-            closest_resource_tile = None
+        if unit.can_act():
             if unit.get_cargo_space_left() > 0:
-                # if the unit is a worker and we have space in cargo, lets find the nearest resource tile and try to mine it
-                for resource_tile in resource_tiles:
-                    if resource_tile.resource.type == Constants.RESOURCE_TYPES.COAL and not player.researched_coal(): continue
-                    if resource_tile.resource.type == Constants.RESOURCE_TYPES.URANIUM and not player.researched_uranium(): continue
-                    dist = resource_tile.pos.distance_to(unit.pos)
-                    if dist < closest_dist:
-                        closest_dist = dist
-                        closest_resource_tile = resource_tile
-                if closest_resource_tile is not None:
-                    actions.append(unit.move(unit.pos.direction_to(closest_resource_tile.pos)))
+                proximal_tile = get_closest_tile(resource_tiles, unit)
+                if proximal_tile:
+                    move_dir = unit.pos.direction_to(proximal_tile.pos)
+                    actions.append(unit.move(move_dir))
             else:
-                # if unit is a worker and there is no cargo space left, and we have cities, lets return to them
-                if len(player.cities) > 0:
-                    closest_dist = math.inf
-                    closest_city_tile = None
-                    for k, city in player.cities.items():
-                        for city_tile in city.citytiles:
-                            dist = city_tile.pos.distance_to(unit.pos)
-                            if dist < closest_dist:
-                                closest_dist = dist
-                                closest_city_tile = city_tile
-                    if closest_city_tile is not None:
-                        move_dir = unit.pos.direction_to(closest_city_tile.pos)
-                        actions.append(unit.move(move_dir))
+                pass
+                
+            # if unit does not have 100 wood, then:
+                # if unit is not adjacent to a wood tile, move towards it
+                # else get resources from wood tile.
+            # if unit is on an empty tile (w/o a city or a resource tile), then create a city
+            # else find shortest path to tile that is empty and then move in that direction
 
-    # you can add debug annotations using the functions in the annotate object
-    # actions.append(annotate.circle(0, 0))
+    # citytile actions
     
     return actions
